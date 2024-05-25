47665f2501302a13ebd5cc2c75db972950f4d77a
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-05-25 12:53:45 +0300

    wo_quotes

--- hw_project/quotes/urls.py
+++ hw_project/quotes/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('<int:page>', views.main, name = "root_paginate"),
     path('detail/<int:quote_id>', views.detail, name='detail'),
     path('tags/<int:tag_id>', views.tags, name='tags'),
+    path('addtag/', views.addtag, name='addtag'),
+    path('addauthor/', views.addauthor, name='addauthor'),
 ]
  
--- hw_project/quotes/views.py
+++ hw_project/quotes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 
 from .models import Author, Tag, Quote
+from .forms import TagForm, AuthorForm
 
 def main(request, page = 1):
     quotes = Quote.objects.all()
@@ -19,3 +20,28 @@ def tags(request, tag_id):
     tag = get_object_or_404(Tag, pk=tag_id)
     quotes = Quote.objects.filter(tags=tag)
     return render(request, 'quotes/tags.html', context={"quotes": quotes, "tag": tag})
+
+def addtag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+        else:
+            return render(request, 'quotes/addtag.html', {'form': form})
+
+    return render(request, 'quotes/addtag.html', {'form': TagForm()})
+
+
+def addauthor(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+        else:
+            return render(request, 'quotes/addauthor.html', {'form': form})
+
+    return render(request, 'quotes/addauthor.html', {'form': AuthorForm()})
+
+
