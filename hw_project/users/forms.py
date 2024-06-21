f3f7874b9b55cf88b2af0827b963cacc468f71b0
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-06-21 11:13:00 +0300

    first

--- hw_project/users/forms.py
+++ hw_project/users/forms.py
@@ -2,9 +2,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+# from .models import Profile
+
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput())
+
+    email = forms.CharField(max_length=100,
                                required=True,
                                widget=forms.TextInput())
 
@@ -25,3 +31,11 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+# class ProfileForm(forms.ModelForm):
+#     avatar = forms.ImageField(widget=forms.FileInput())
+#
+#     class Meta:
+#         model = Profile
+#         fields = ['avatar']
